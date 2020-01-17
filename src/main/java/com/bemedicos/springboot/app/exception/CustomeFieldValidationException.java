033d02fe4e9b575d9830e515608ab7a7cb3acddb
carlos151195 <eliosaamarocarlos@gmail.com>
2020-01-17 12:54:40 -0600

    Add files via upload

---
+++ src/main/java/com/bemedicos/springboot/app/exception/CustomeFieldValidationException.java
@@ -0,0 +1,19 @@
+package com.medicos.springboot.app.exception;
+
+public class CustomeFieldValidationException extends Exception{
+	
+private static final long serialVersionUID = -4995433707591853255L;
+	
+	private String fieldName;
+	
+	public CustomeFieldValidationException(String message, String fieldName) {
+		super(message);
+		this.fieldName = fieldName;
+	}
+	
+	public String getFieldName() {
+		return this.fieldName;
+	}
+	
+
+}
