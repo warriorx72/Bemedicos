e294e87b1fee71ed8f753e94c7ea5dfd8528a98d
carlos151195 <eliosaamarocarlos@gmail.com>
2020-01-17 12:56:24 -0600

    Add files via upload

---
+++ src/main/java/com/bemedicos/springboot/app/util/Passgenerator.java
@@ -0,0 +1,13 @@
+package com.medicos.springboot.app.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class Passgenerator {
+	
+//	public static void main(String ...args) {
+//        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(4);
+//        
+//        System.out.println(bCryptPasswordEncoder.encode("1234"));
+//	}
+
+}
