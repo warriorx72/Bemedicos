137a78ce3f298fa726583d3d479a8e7614d108a0
carlos151195 <eliosaamarocarlos@gmail.com>
2020-01-17 13:04:08 -0600

    Update UserRepository.java

--- src/main/java/com/bemedicos/springboot/app/repository/UserRepository.java
+++ src/main/java/com/bemedicos/springboot/app/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
-import com.medicos.springboot.app.models.entity.User;
+import com.bemedicos.springboot.app.models.entity.User;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long>{
