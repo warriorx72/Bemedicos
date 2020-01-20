899d55a7330ce09f636ecd9ad81747a2eb8afd3e
Uriel Cuatepitzi Flores <warriorx72@gmail.com>
2020-01-20 15:53:16 -0600

    Persona new entity

--- src/main/java/com/bemedicos/springboot/app/models/entity/Persona.java
+++ src/main/java/com/bemedicos/springboot/app/models/entity/Persona.java
@@ -18,8 +18,6 @@ import javax.persistence.Table;
 public class Persona implements Serializable{
 
 	private static final long serialVersionUID = 1L;
-
-	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long persona_id;
