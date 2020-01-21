ba6490ae0552298e557ee09206b5740756132e6d
carlos151195 <eliosaamarocarlos@gmail.com>
2020-01-21 17:38:20 -0600

    Update UserController.java

--- src/main/java/com/bemedicos/springboot/app/controllers/UserController.java
+++ src/main/java/com/bemedicos/springboot/app/controllers/UserController.java
@@ -1,10 +1,15 @@
 package com.bemedicos.springboot.app.controllers;
+import java.util.Enumeration;
 import java.util.stream.Collectors;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 import javax.validation.Valid;
 
+import org.apache.catalina.connector.Request;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.RequestPath;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -15,11 +20,20 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.support.RequestPartServletServerHttpRequest;
 
 import com.bemedicos.springboot.app.dto.ChangePasswordForm;
+import com.bemedicos.springboot.app.exception.CustomeFieldValidationException;
+import com.bemedicos.springboot.app.models.entity.Direccion;
+import com.bemedicos.springboot.app.models.entity.Medicos;
+import com.bemedicos.springboot.app.models.entity.Persona;
 import com.bemedicos.springboot.app.models.entity.User;
 import com.bemedicos.springboot.app.repository.UserRepository;
+import com.bemedicos.springboot.app.service.DireccionService;
 import com.bemedicos.springboot.app.service.MedicoService;
+import com.bemedicos.springboot.app.service.PersonaService;
 import com.bemedicos.springboot.app.service.UserService;
 
 
@@ -37,27 +51,77 @@ public class UserController {
 	UserService userService;
 	
 	@Autowired
-	MedicoService medicosDao;
+	MedicoService medicoDao;
+	
+	@Autowired
+	DireccionService direccionDao;
+	
+	@Autowired
+	PersonaService personaDao;
+
+	
+	
+	
+	
 	
 	@GetMapping("/signup")
 	public String signup(Model model) {
 		
 		model.addAttribute("signup",true);
 		model.addAttribute("userForm", new User());
+		model.addAttribute("direccion", new Direccion());
+		model.addAttribute("persona", new Persona());
+		model.addAttribute("medico", new Medicos());
 		return "user-form/user-signup";
 	}
 	
 	
+	
+	
 	@PostMapping("/signup")
-   	public String postSignup(@Valid @ModelAttribute("userForm")User user, BindingResult result, ModelMap model) {
+   	public String postSignup(@Valid @ModelAttribute("direccion")Direccion direccion,@ModelAttribute("persona")Persona persona, @ModelAttribute("medico")Medicos medico, @ModelAttribute("userForm")User user, BindingResult result, ModelMap model) {
+	
 		model.addAttribute("userForm", user);
+	
 		model.addAttribute("signup",true);
 		if(result.hasErrors()) {
 			return "user-form/user-signup";
 		}else {
 			try {
+				///user.setMedico_id(2);
 				userService.createUser(user);
+				///direccion.setDireccionCalle("independencia");
+		/////sirve		direccionDao.save(direccion);
+		////sirve		persona.setPersona_nombre(user.getFirstName());
+		////sirve		persona.setPersona_ap(user.getLastName());
+				////persona.setPersonaAm("lopez");
+				///persona.setIdDireccion(direccion.getDireccionId());
+		///sirve		personaDao.save(persona);
+				////medico.setMedico_cedula("dfasp");
+				///medico.setPersona_id(persona.getPersonaId());
+				direccion.setPersona(persona);
+				persona.setDireccion(direccion);
+				persona.setMedicos(medico);
+				persona.setPersona_nombre(user.getFirstName());
+				persona.setPersona_ap(user.getLastName());
+				medico.setPersona(persona);
+				direccionDao.save(direccion);
+			////sirve	medicoDao.save(medico);
+				userService.updateUser(user);
+			   user.setMedico_id(medico.getMedico_id());
+			  //// System.out.println(medico.getMedico_id());
+			   userService.updateUser(user);
+		        
+				
+				
+				
+				
+				
 			} 
+			catch (CustomeFieldValidationException cfve) {
+				result.rejectValue(cfve.getFieldName(), null, cfve.getMessage());
+				return "user-form/user-signup";
+			}
 			catch (Exception e) {
 				model.addAttribute("formErrorMessage",e.getMessage());
 				return "user-form/user-signup";
@@ -69,21 +133,66 @@ public class UserController {
 	@GetMapping({"/","/login"})
 	public String index() {
 		return "login";
-	}//End
+	}
 	
 	@GetMapping("/index")
-	public String getUserForm(@Valid @ModelAttribute("userForm")User user, BindingResult result, ModelMap model) {
-	
+	public String getUserForm(HttpServletRequest request,@ModelAttribute("userForm")User user, BindingResult result, ModelMap model) {
 	   
+		///System.out.println("usuario doctor"+UsuarioDoctor(req));
+	 System.out.println(UsuarioDoctor(request,userService)); 
+	  
+		//UsuarioDoctor(req);
+	  //userService.getIdDoc(UsuarioDoctor(req));
+	  //System.out.println(userService.getIdDoc(UsuarioDoctor(req)).toArray());
+	  //Object[] idD=userService.getIdDoc(UsuarioDoctor(req)).toArray();
+	  ///Object[] idD2=idD[0];
+	  //System.out.println("el ides"+idD[0]);
+	  //Integer idDoct=idD[0].hashCode();
+	  //System.out.println(idp);
+	 /// System.out.println("hola"+test(request));
+	
+	 // System.out.println(idD2[0]);
+	  //String IdDoctor23=idD2[0].toString();
+	  //System.out.println("EL ID D"+IdDoctor23); 
+	 /// System.out.println(IdDoctor2);
+	 model.addAttribute("id_med_user",UsuarioDoctor(request,userService));
 		model.addAttribute("userForm", new User());
 		
 		model.addAttribute("userList", userService.getAllUsers());
 		model.addAttribute("listTab","active");
+	
+
+		///httpServletRequest.UserPrincipal.Name
 		return "index";
 	}//End	
 	
+	public String hola() {
+		
+		String hola="hola mundo";
+		return hola;
+		
+	}
+	
+	
+	public Integer UsuarioDoctor(HttpServletRequest request,UserService userService) {
+		String usuarioDoctor= request.getUserPrincipal().getName();
+		///System.out.println("el id es"+IdDoctor2);
+		////return IdDoctor2;
+		
+		////-------------------
+		 
+		  userService.getIdDoc(usuarioDoctor);
+		  Object[] idD=userService.getIdDoc(usuarioDoctor).toArray();
+		  Integer idDoct=idD[0].hashCode();
+		 //// System.out.println("metodo sin return"+idDoct);
+	return idDoct;
+		
+	}
+	
+	
 	@PostMapping("/index")
-	public String postUserForm(@Valid @ModelAttribute("userForm")User user, BindingResult result, ModelMap model) {
+	public String postUserForm( @ModelAttribute("userForm")User user, BindingResult result, ModelMap model) {
+		
 		if(result.hasErrors()) {
 			model.addAttribute("userForm", user);
 			model.addAttribute("formTab","active");
@@ -92,16 +201,27 @@ public class UserController {
 				userService.createUser(user);
 				model.addAttribute("userForm", new User());
 				model.addAttribute("listTab","active");
+				
 			} catch (Exception e) {
 				model.addAttribute("formErrorMessage",e.getMessage());
 				model.addAttribute("userForm", user);
 				model.addAttribute("formTab","active");
 				model.addAttribute("userList", userService.getAllUsers());
+				
 			}
+			
 		}//End_else
 		model.addAttribute("userList", userService.getAllUsers());
+		System.out.println(model.addAttribute("userList", userService.getAllUsers()));
+		
 		return "index";
 	}//End
+	
+	public void imprimir(String email) {
+		System.out.println(email+"holamundo");
+		
+	}
+	
 	
 	@GetMapping("/editUser/{id}")
 	public String getEditUserForm(Model model, @PathVariable(name ="id")Long id)throws Exception{
