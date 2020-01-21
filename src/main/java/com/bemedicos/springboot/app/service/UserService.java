e64cc5d70c40ed837aa6606632bdf1059653af7e
carlos151195 <eliosaamarocarlos@gmail.com>
2020-01-21 17:53:02 -0600

    Update UserService.java

--- src/main/java/com/bemedicos/springboot/app/service/UserService.java
+++ src/main/java/com/bemedicos/springboot/app/service/UserService.java
@@ -2,6 +2,8 @@ package com.bemedicos.springboot.app.service;
 
 
 
+import java.util.List;
+
 import com.bemedicos.springboot.app.dto.ChangePasswordForm;
 import com.bemedicos.springboot.app.models.entity.User;
 
@@ -18,6 +20,8 @@ public interface UserService {
 	public User updateUser(User user) throws Exception;
 	
 	public User changePassword(ChangePasswordForm form) throws Exception;
+
+	public List<Object> getIdDoc(String usuarioDoctor);
 
 	
 
