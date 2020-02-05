b7671e242f3afef3c28ddfde68ad72a39fd4c1c8
chacervantes <59617173+chacervantes@users.noreply.github.com>
2020-02-05 11:39:45 -0600

    UploadFileService

---
+++ src/main/java/com/bemedicos/springboot/app/service/UploadFileService.java
@@ -0,0 +1,17 @@
+package com.bemedicos.springboot.app.service;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface UploadFileService {
+	
+	public Resource load(String filename) throws MalformedURLException;
+	
+	public String copy(MultipartFile file) throws IOException;
+	
+	public boolean delete(String filename);
+
+}
---
+++ src/main/java/com/bemedicos/springboot/app/service/UploadFileServiceImpl.java
@@ -0,0 +1,57 @@
+package com.bemedicos.springboot.app.service;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class UploadFileServiceImpl implements UploadFileService {
+
+	private final static String carpetaUploads = "uploads";
+
+	@Override
+	public Resource load(String filename) throws MalformedURLException {
+		Path pathFoto = getPath(filename);
+		Resource recurso = null;
+
+		recurso = new UrlResource(pathFoto.toUri());
+
+		if (!recurso.exists() || !recurso.isReadable()) {
+			throw new RuntimeException("Error: no se puede cargar la imagen " + pathFoto.toString());
+		}
+		return recurso;
+	}
+
+	@Override
+	public String copy(MultipartFile file) throws IOException {
+		String uniqueFileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
+		Path rootPath = getPath(uniqueFileName);
+
+		Files.copy(file.getInputStream(), rootPath);
+
+		return uniqueFileName;
+	}
+
+	@Override
+	public boolean delete(String filename) {
+		Path rootPath = getPath(filename);
+		File archivo = rootPath.toFile();
+
+		if (archivo.exists() && archivo.canRead()) {
+			archivo.delete();
+		}		return true;
+	}
+
+	public Path getPath(String filename) {
+		return Paths.get(carpetaUploads).resolve(filename).toAbsolutePath();
+	}
+}
