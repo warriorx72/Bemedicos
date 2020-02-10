e1faa07adb2b03b7769e76c777035ba23e9b7e1c
chacervantes <59617173+chacervantes@users.noreply.github.com>
2020-02-10 17:47:47 -0600

    horariorepository

---
+++ src/main/java/com/bemedicos/springboot/app/repository/HorarioRepository.java
@@ -0,0 +1,9 @@
+package com.bemedicos.springboot.app.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bemedicos.springboot.app.models.entity.Horario;
+
+public interface HorarioRepository extends CrudRepository<Horario, Long> {
+
+}
