e8b6226bf88761c246858953a2b09b8f3313bf22
Uriel Cuatepitzi Flores <warriorx72@gmail.com>
2020-02-17 10:43:26 -0600

    Error

--- src/main/java/com/bemedicos/springboot/app/service/UserDetailsServiceImpl.java
+++ src/main/java/com/bemedicos/springboot/app/service/UserDetailsServiceImpl.java
@@ -18,11 +18,10 @@ import com.bemedicos.springboot.app.repository.UserRepository;
 
 @Service
 @Transactional
-public class UserDetailsServiceImpl implements UserDetailsService{
+public class UserDetailsServiceImpl implements UserDetailsService {
 
-	
 	@Autowired
-    UserRepository repository;
+	UserRepository repository;
 
 //	@Override
 //	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -36,65 +35,47 @@ public class UserDetailsServiceImpl implements UserDetailsService{
 //	
 //		return user;
 //	}
-@Override
-public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		
-		if(username.contains("@")) {
-		
-com.bemedicos.springboot.app.models.entity.User appUser = repository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Login username or email ivalidos"));
-			
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		if (username.contains("@")) {
+
+			com.bemedicos.springboot.app.models.entity.User appUser = repository.findByEmail(username)
+					.orElseThrow(() -> new UsernameNotFoundException("Login username or email ivalidos"));
+			int status=(appUser.getUser_med_status());
+			if (status == 0 || status != 1) {
+				System.out.println("adios");
+				System.out.println(status);
+				throw new UsernameNotFoundException("Usuario no activo.");
+
+			}
 			Set<GrantedAuthority> grantList = new HashSet<GrantedAuthority>();
-		UserDetails user = (UserDetails) new User (username,appUser.getPassword(), grantList);
-		return user;
-		}
-		else
-		{
-com.bemedicos.springboot.app.models.entity.User appUser = repository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Login username or email ivalidos"));
-			
+			UserDetails user = (UserDetails) new User(username, appUser.getPassword(), grantList);
+			return user;
+		} else {
+			com.bemedicos.springboot.app.models.entity.User appUser = repository.findByUsername(username)
+					.orElseThrow(() -> new UsernameNotFoundException("Login username or email ivalidos"));
+			int status=(appUser.getUser_med_status());
+			if (status == 0 || status != 1) {
+				System.out.println("adios");
+				System.out.println(status);
+				throw new UsernameNotFoundException("Usuario no activo.");
+
+			}
 			Set<GrantedAuthority> grantList = new HashSet<GrantedAuthority>();
-		UserDetails user = (UserDetails) new User (username,appUser.getPassword(), grantList);
-		return user;
-		
-		
+			UserDetails user = (UserDetails) new User(username, appUser.getPassword(), grantList);
+			return user;
+
 		}
-		
-		
-		
-		
-		
-		
-		
-		
-	/*	for(Role role: appUser.getRoles()) {
-			GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(role.getDescription());
-			grantList.add(grantedAuthority);
-		}  */
-		
-		
-	
-		
-	
-		
-		
-		
+
+		/*
+		 * for(Role role: appUser.getRoles()) { GrantedAuthority grantedAuthority = new
+		 * SimpleGrantedAuthority(role.getDescription());
+		 * grantList.add(grantedAuthority); }
+		 */
+
 	}
 
-
-
-
-
-
-	
-
-
-
-	
-	
-	
-
-
-
-	
 //	@Override
 //	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 //
@@ -110,6 +91,3 @@ com.bemedicos.springboot.app.models.entity.User appUser = repository.findByUsern
 //		return user;
 //	}
 }
-
-
-
