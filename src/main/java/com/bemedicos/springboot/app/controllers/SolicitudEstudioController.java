40e26c30b0625835b9548f69adee4442128e82e1
DTI <DTI@DESKTOP-3HJOK2H.mshome.net>
2020-02-26 12:22:48 -0600

    pdf: aparece nombre de medico

--- src/main/java/com/bemedicos/springboot/app/controllers/SolicitudEstudioController.java
+++ src/main/java/com/bemedicos/springboot/app/controllers/SolicitudEstudioController.java
@@ -22,52 +22,48 @@ import com.bemedicos.springboot.app.service.PacienteService;
 import com.bemedicos.springboot.app.service.PersonaService;
 import com.bemedicos.springboot.app.service.UserService;
 
-
 @Controller
-public class SolicitudEstudioController 
-{	
-	
+public class SolicitudEstudioController {
+
 	@Autowired
 	private PacienteService pacienteService;
-	
+
 	@Autowired
 	private EstudioService estudioService;
-	
+
 	@Autowired
 	private MedicoService medicoService;
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@Autowired
 	private PersonaService personaService;
-	
-	@RequestMapping(value="/solicitud_estudios/{id}", method=RequestMethod.GET)
-	public String listarNotas(HttpServletRequest request, @PathVariable(value="id") Long id, Map<String, Object> model,Model m) 
-	{
+
+	@RequestMapping(value = "/solicitud_estudios/{id}", method = RequestMethod.GET)
+	public String listarNotas(HttpServletRequest request, @PathVariable(value = "id") Long id, Map<String, Object> model,
+			Model m) {
 		Paciente paciente = new Paciente();
 		Persona per = new Persona();
 		UserController us = new UserController();
-		
-		us.UsuarioDoctor(request,userService);
-		Medicos med = medicoService.findOne(Long.valueOf(us.UsuarioDoctor(request,userService)));
-		per = personaService.findOne(Long.valueOf(med.getMedico_id()));
-		
-		if(id>0) 
-		{
+
+		us.UsuarioDoctor(request, userService);
+		Medicos med = medicoService.findOne(Long.valueOf(us.UsuarioDoctor(request, userService)));
+
+		per = personaService.findOne(Long.valueOf(med.getPersona().getPersona_id()));
+		System.out.println(per + " SDFGHJKJHGF " + per.getPersona_nombre());
+		if (id > 0) {
 			paciente = pacienteService.findOne(id);
-		}
-		else 
-		{
+		} else {
 			return "redirect:historial_clinico";
 		}
-		
+
 		m.addAttribute("paciente", paciente.getPersona());
 		m.addAttribute("estudio", estudioService.findAll());
 		m.addAttribute("id_paciente", id);
 		m.addAttribute("medico_info", med);
 		m.addAttribute("medico_info_persona", per);
-		
-		return "solicitud_estudios1";	
+
+		return "solicitud_estudios1";
 	}
 }
